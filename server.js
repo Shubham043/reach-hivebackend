05b42317c12a61280134f9fab912ebb7a18b2fd9
Shubham Sharma <rawanshubham@gmail.com>
2023-04-09 01:49:27 +0530

    cors added

--- server.js
+++ server.js
@@ -1,28 +1,29 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import routes from './routes/routes.js'
-// import connectDB from './database/db.js';
+import cors from 'cors';
 
 const uri = "mongodb+srv://rawanshubham:5Y0tex1Xb8xfiIKh@cluster0.hbfn6ar.mongodb.net/?retryWrites=true&w=majority";
-// const uri ="mongodb://127.0.0.1:27017/myapp";
-
 const app = express();
 const port = 8000;
-mongoose.connect(uri , {
-    // useNewUriParser: true,
-    useUnifiedTopology: true
-  }).then(()=>{
-    console.log('connected to database');
-  }).catch((e)=>{
-    console.log(e)
-  });
 
-// use routes
+mongoose.connect(uri, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+}).then(() => {
+  console.log('Connected to database');
+}).catch((e) => {
+  console.log(e)
+});
+
+// Use CORS middleware
+app.use(cors());
+
+// Use routes
 app.use(express.json());
 app.use('/api', routes);
 
-// start the server
-
+// Start the server
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-  });
+  console.log(`Server is running at http://localhost:${port}`);
+});
